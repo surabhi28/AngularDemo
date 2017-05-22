059cc9f04bed623093475c2b56d9197d2e16506f
surabhi28 <dakoliya.surabhi@gmail.com>
2017-05-22 18:31:57 +0530

    x

---
+++ app/models/user.js
@@ -0,0 +1,15 @@
+var crypto = require('crypto');
+
+var userSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    unique: true,
+    required: true
+  },
+  name: {
+    type: String,
+    required: true
+  },
+  hash: String,
+  salt: String
+});
